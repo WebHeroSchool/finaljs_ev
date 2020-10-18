9f2c115a0fa6147cd4a98425a358b76344f31edc
Владимирова Екатерина <echapyrkina@mail.ru>
2020-10-18 17:38:39 +0400

    changes

---
+++ script.js
@@ -0,0 +1,84 @@
+const button = document.getElementById('mainmenu__button_btn');
+const level = document.getElementById('mainmenu__level');
+const mainmenu = document.getElementById('mainmenu');
+const firstlevel = document.getElementById('firstlevel');
+const secondlevel = document.getElementById('secondlevel');
+const thirdlevel = document.getElementById('thirdlevel');
+const card = document.querySelectorAll('.card__img');
+const cardFront = document.querySelectorAll('.card__front');
+const cardBack = document.querySelectorAll('.card__back');
+button.addEventListener('click', event => {
+		if (level[0].checked) {
+			mainmenu.classList.toggle('mainmenu__display');
+			firstlevel.classList.remove('firstlevel__display');
+			firstlevel.classList.add('firstlevel');
+			playGame('.card__back_imgfirstlevel', 4, 2);
+		}
+		else if (level[1].checked) {
+			mainmenu.classList.remove('mainmenu');
+			mainmenu.classList.add('mainmenu__display');
+			firstlevel.classList.add('firstlevel__display');
+			firstlevel.classList.remove('firstlevel');
+			secondlevel.classList.remove('secondlevel__display');
+			secondlevel.classList.add('secondlevel');
+			playGame('.card__back_imgsecondlevel', 7, 5);
+		}
+		else if (level[2].checked) {
+			mainmenu.classList.remove('mainmenu');
+			mainmenu.classList.add('mainmenu__display');
+			firstlevel.classList.add('firstlevel__display');
+			firstlevel.classList.remove('firstlevel');
+			secondlevel.classList.add('secondlevel__display');
+			secondlevel.classList.remove('secondlevel');
+			thirdlevel.classList.remove('thirdlevel__display');
+			thirdlevel.classList.add('thirdlevel');
+			playGame('.card__back_imgthirdlevel', 11, 9)
+	}
+	});
+
+function playGame(nameClass, num, max) {
+	const cardImages= [];
+	const cardBackImages = document.querySelectorAll(nameClass);
+	card.forEach((item, m) => {
+		card[m].onmouseover = () => cardFront[m].classList.add('card_hover');
+		card[m].onmouseout = () => cardFront[m].classList.remove('card_hover');
+	});
+let min = 0;
+x = Math.floor(Math.random() * (max - min + 1)) + min;
+for(let i = 0; i < num - 1; i++) {
+	if(i === x) {
+		cardImages.push('./img/card_BUG.png');
+	} else cardImages.push('./img/card_gameover.png');
+};
+cardBackImages.forEach((elem, i) => {
+	cardBackImages[i].src = cardImages[i];
+});
+}
+
+card.forEach((item, n) => {
+	card[n].addEventListener('click', event => {
+		card[n].classList.add('card__flip');
+	cardBack[n].classList.remove('card__back');
+	cardFront[n].classList.add('card__back');
+	element.className = 'newelement';
+	document.body.appendChild(element);
+	element.addEventListener('click', click);
+	});
+});
+const element = document.createElement('div');
+const click =() => {
+	mainmenu.classList.remove('mainmenu__display');
+	firstlevel.classList.add('firstlevel__display');
+	firstlevel.classList.remove('firstlevel');
+	secondlevel.classList.add('secondlevel__display');
+	secondlevel.classList.remove('secondlevel');
+	thirdlevel.classList.add('thirdlevel__display');
+	thirdlevel.classList.remove('thirdlevel');
+	document.body.removeChild(element);
+	card.forEach((item, index) => {
+		card[index].classList.remove('card__flip');
+		cardBack[index].classList.add('card__back');
+		cardFront[index].classList.remove('card__back');
+		card[index].removeEventListener('click', click);
+	});
+}
